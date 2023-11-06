eaf3021df8e3a541551db2be7b1c347187ecaaea
Peter <peter@boringhex.top>
2023-11-06 17:32:25 +0800

    test: 测试mpu6050
    Changes to be committed:
    	modified:   main/main.c

--- main/main.c
+++ main/main.c
@@ -9,11 +9,12 @@
 #include "hp203.h"
 #include "mpu6050.h"
 
-const static char *TAG = "I2C_EXAMPLE";
+static const char *TAG = "I2C_EXAMPLE";
+static mpu6050_handle_t mpu6050 = NULL;
 
 #define I2C_MASTER_SCL_IO 8         /*!< GPIO number used for I2C master clock */
 #define I2C_MASTER_SDA_IO 9         /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM 0            /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_NUM I2C_NUM_0    /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
 #define I2C_MASTER_FREQ_HZ 400000   /*!< I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
@@ -40,12 +41,34 @@ static esp_err_t i2c_master_init(void)
     return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
 }
 
+/**
+ * @brief i2c master initialization
+ */
+static void mpu6050_init(void)
+{
+    esp_err_t ret;
+
+    mpu6050 = mpu6050_create(I2C_MASTER_NUM, MPU6050_I2C_ADDRESS);
+
+    ESP_ERROR_CHECK(mpu6050_config(mpu6050, ACCE_FS_4G, GYRO_FS_500DPS));
+
+    ESP_ERROR_CHECK(mpu6050_wake_up(mpu6050));
+}
+
 void app_main(void)
 {
     float tmp, pressure, temperature, altitude[4], avg;
     int i = 0;
+
+    esp_err_t ret;
+    uint8_t mpu6050_deviceid;
+    mpu6050_acce_value_t acce;
+    mpu6050_gyro_value_t gyro;
+    mpu6050_temp_value_t temp;
+
     i2c_master_init();
     hp203b_init();
+    mpu6050_init();
 
     ESP_ERROR_CHECK(hp203b_read_altitude(&altitude[0]));
     ESP_ERROR_CHECK(hp203b_read_altitude(&altitude[1]));
@@ -72,6 +95,21 @@ void app_main(void)
         ESP_LOGI(TAG, "Altitude: %f", avg);
 
         i++;
+
+        ret = mpu6050_get_deviceid(mpu6050, &mpu6050_deviceid);
+        ESP_LOGI(TAG, "device id:%X", mpu6050_deviceid);
+
+        ret = mpu6050_get_acce(mpu6050, &acce);
+        ESP_LOGI(TAG, "acce_x:%.2f, acce_y:%.2f, acce_z:%.2f\n", acce.acce_x, acce.acce_y, acce.acce_z);
+
+        ret = mpu6050_get_gyro(mpu6050, &gyro);
+        ESP_LOGI(TAG, "gyro_x:%.2f, gyro_y:%.2f, gyro_z:%.2f\n", gyro.gyro_x, gyro.gyro_y, gyro.gyro_z);
+
+        ret = mpu6050_get_temp(mpu6050, &temp);
+        ESP_LOGI(TAG, "t:%.2f \n", temp.temp);
+
         vTaskDelay(pdMS_TO_TICKS(500));
     }
+    mpu6050_delete(mpu6050);
+    ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
 }
